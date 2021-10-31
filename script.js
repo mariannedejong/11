e05761da4f8d5faf3f7ca35dfba5cc471f02e4bb
mariannedejong <89845904+mariannedejong@users.noreply.github.com>
2021-10-31 23:41:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello Winc Academy");
+let naam = "Marianne";
+console.log(naam);
+console.log(4+4);
+console.log("4"+"4");
+console.log("4+4");
+console.log(12*4);
+
+let leeftijdInCijfers = 36;
+let leeftijdAlsString= "36";
